8138431a0f1800d0bd05a78a3ce2609b7df2abf4
Ana Carolina Almeida <ana.carolina.almeida.30@gmail.com>
2023-04-30 19:03:17 -0300

    Creating ChessPosition Class

--- Chess/Chess/Program.cs
+++ Chess/Chess/Program.cs
@@ -7,19 +7,27 @@ namespace Chess
     {
         static void Main()
         {
+            /*
             try
             {
+                
                 Board board = new Board(8, 8);
                 board.IncludePiece(new Rook(Color.Black, board), new Position(0, 0));
                 board.IncludePiece(new Rook(Color.Black, board), new Position(1, 3));
                 board.IncludePiece(new King(Color.Black, board), new Position(2, 4));
 
                 Screen.PrintBoard(board);
+                
             }
             catch(BoardException ex) 
             {
                 Console.WriteLine(ex.Message);
             }
+            */
+
+            ChessPosition position = new ChessPosition('c', 7);
+            Console.WriteLine(position);
+            Console.WriteLine(position.ToPosition());
         }
     }
 }
